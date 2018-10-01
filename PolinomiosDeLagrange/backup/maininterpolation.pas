dec118d23d1d8b1d4acd98d7c1f82f166bafd39c
Fabrizio <fabrizio.flores@ucsp.edu.pe>
2018-09-30 23:14:24 -0500

    Code

--- PolinomiosDeLagrange/backup/maininterpolation.pas
+++ PolinomiosDeLagrange/backup/maininterpolation.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, TAGraph, TAFuncSeries, TASeries, TATools, Forms,
-  Controls, Graphics, Dialogs, StdCtrls, Grids, LogicInterpolation;
+  Controls, Graphics, Dialogs, StdCtrls, Grids, LogicInterpolation ;
 
 type
 
